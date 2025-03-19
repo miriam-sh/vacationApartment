bd724db5022627931ccd3e3a66f99101b7c900ae
miriam-sh <m0534106023@Gmail.com>
2025-03-19 17:25:21 +0200

    Delete כל מיני directory

--- כל מיני/src1/VacationApartment/data/gets.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const getCategories = async () => {
-    return axios.get("category");
-};
-
-export const getCities = async () => {
-    return axios.get("city");
-}

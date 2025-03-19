c1f2c0a9e203e1f621256b2f39ab8b5b169437fc
miriam-sh <m0534106023@Gmail.com>
2025-03-19 17:25:21 +0200

    Delete כל מיני directory

--- כל מיני/projectOnki/קבצים של הפרוייקט מעורבים ממגוון שלבים/apartments_project/api/models/image.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const imageSchema = new mongoose.Schema({
-  data: Buffer,
-  contentType: String,
-});
-
- export default mongoose.model('Image', imageSchema);
-
